757d9d0ba3791a3b47c8052cd868264d1ed2ad49
cit-dev2026 <joaquim.coimbra@cit-bs.co.za>
2026-07-09 10:38:45 +0000

    Add HR login page component

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -3,9 +3,14 @@ import { createBrowserRouter, Navigate } from 'react-router-dom'
 import { AppLayout } from '@/layouts/app-layout'
 import { DashboardPage } from '@/pages/dashboard-page'
 import { EmployeesPage } from '@/pages/employees-page'
+import { LoginPage } from '@/pages/login-page'
 import { NotFoundPage } from '@/pages/not-found-page'
 
 export const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
   {
     path: '/',
     element: <AppLayout />,
